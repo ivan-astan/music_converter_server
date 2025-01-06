3e40d51ed8d59cec52dfa73ab4de307c56194259
ivan-astan <astanin.i.a@gmail.com>
2025-01-06 20:32:29 +0300

    fix

--- app/api/music.py
+++ app/api/music.py
@@ -1,47 +1,48 @@
 from fastapi import APIRouter, UploadFile, File, Response, Request
 from typing import List
 from pydub import AudioSegment
-import os, subprocess
+from midi2audio import FluidSynth
+import os
 from app.database.connection import database
-
 
 router = APIRouter()
 
 @router.post("/music_converter/{userId}")
 async def convert_music(request: Request, userId: int, files: List[UploadFile] = File(...)):
     soundfont_path = os.path.join(os.path.dirname(__file__), 'GeneralUser-GS.sf2')
-    wav_files = []
+    fs = FluidSynth(soundfont_path)
 
     output_directory = "output"
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
+    wav_files = []
+
     for file in files:
         contents = await file.read()
-        
         temp_midi_path = os.path.join(output_directory, file.filename)
+
         with open(temp_midi_path, 'wb') as f:
             f.write(contents)
 
-        output_format = 'wav'
-        wav_filename = f"{os.path.splitext(file.filename)[0]}.{output_format}"
+        wav_filename = f"{os.path.splitext(file.filename)[0]}.wav"
         wav_file_path = os.path.join(output_directory, wav_filename)
 
-       
         try:
-            subprocess.run(['fluidsynth', '-ni', soundfont_path, temp_midi_path, '-F', wav_file_path, '-n', 'audio.file-format=wav'], check=True)
-            wav_files.append(wav_file_path) 
-        except subprocess.CalledProcessError as e:
+            fs.midi_to_audio(temp_midi_path, wav_file_path)
+            wav_files.append(wav_file_path)
+        except Exception as e:
             return {"message": str(e), "error": True}
         finally:
-            os.remove(temp_midi_path)  
+            os.remove(temp_midi_path)
+
 
     combined = AudioSegment.empty()
     for wav_file in wav_files:
         audio_segment = AudioSegment.from_wav(wav_file)
         combined += audio_segment 
 
-    output_filename = f"combined_output.wav"
+    output_filename = "combined_output.wav"
     output_path = os.path.join(output_directory, output_filename)
 
     try:
@@ -54,10 +55,10 @@ async def convert_music(request: Request, userId: int, files: List[UploadFile] =
 
     with open(output_path, 'rb') as f:
         music_data = f.read()
-    
+
     query = "INSERT INTO history (userid, music) VALUES (:userid, :music) RETURNING id"
     id = await database.execute(query=query, values={"userid": userId, "music": music_data})
-    
+
     url = f"{request.url.scheme}://{request.url.netloc}/music/{id}"
 
     update_query = "UPDATE history SET url = :url WHERE id = :id"
@@ -78,7 +79,7 @@ async def get_file(id: int):
     music_data = music_record["music"]
 
     return Response(content=music_data, media_type="audio/wav")
-    
+
 @router.get("/history/{userId}")
 async def get_history(userId: int, page: int = 1, pageSize: int = 3):
     if page > 0:
@@ -102,6 +103,7 @@ async def get_history(userId: int, page: int = 1, pageSize: int = 3):
         "totalCount": total_count,
         "totalPages": total_pages
     }
+
 @router.delete("/history/{userId}/{id}")
 async def delete_file(userId: int, id: int, pageSize: int = 3): 
     query = "DELETE FROM history WHERE id = :id"
@@ -118,4 +120,3 @@ async def delete_file(userId: int, id: int, pageSize: int = 3):
         return {"error": False, "message": "Successful", "totalPages": total_pages, "totalCount": total_count}
     except Exception as e:
         return {"error": True, "message": str(e)}
-    
